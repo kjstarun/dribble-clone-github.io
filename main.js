0a1575326045d2d23d9dba53776be5c9a68b9c13
kjstarun <kjstarun@gmail.com>
2022-12-06 18:50:13 +0530

    recreating using js

--- main.js
+++ main.js
@@ -1,56 +1,56 @@
-// const slidesContainer = document.getElementById("slides");
+const slidesContainer = document.getElementById("slides");
 
-// //creating parent container 
-// const parentContainer = document.createElement("div");
-// parentContainer.className = "parent-container";
+//creating parent container 
+const parentContainer = document.createElement("div");
+parentContainer.className = "parent-container";
 
-// // creating image contaienr
-// const imageContainer = document.createElement('div');
-// imageContainer.className = "image-container";
-// const image = document.createElement("img");
-// image.src = "./realistic-happy-birthday-black-golden_1361-3241.webp"; 
+// creating image contaienr
+const imageContainer = document.createElement('div');
+imageContainer.className = "image-container";
+const image = document.createElement("img");
+image.src = "./realistic-happy-birthday-black-golden_1361-3241.webp"; 
 
-// // appending image to image container
-// imageContainer.appendChild(image);
+// appending image to image container
+imageContainer.appendChild(image);
 
-// // creating details container
-// const detailsContainer = document.createElement("div");
-// detailsContainer.className = "details-container";
+// creating details container
+const detailsContainer = document.createElement("div");
+detailsContainer.className = "details-container";
 
-// // creating name container
-// const nameContainer = document.createElement("div");
-// nameContainer.className = "name-container";
+// creating name container
+const nameContainer = document.createElement("div");
+nameContainer.className = "name-container";
 
-// // creating the logo
-// const logoImage = document.createElement("img");
-// logoImage.src = "./heart.svg";
+// creating the logo
+const logoImage = document.createElement("img");
+logoImage.src = "./heart.svg";
 
-// // creating image title
-// const imageTitle = document.createElement("p");
-// imageTitle.id = "title";
-// imageTitle.textContent = "TARUN";
+// creating image title
+const imageTitle = document.createElement("p");
+imageTitle.id = "title";
+imageTitle.textContent = "TARUN";
 
-// // appending the title and images to name container
-// nameContainer.appendChild(logoImage);
-// nameContainer.appendChild(imageTitle);
+// appending the title and images to name container
+nameContainer.appendChild(logoImage);
+nameContainer.appendChild(imageTitle);
 
-// // appending name container to details container
-// detailsContainer.appendChild(nameContainer);
+// appending name container to details container
+detailsContainer.appendChild(nameContainer);
 
-// // creating reach container
-// const reachContainer = document.createElement("div");
-// reachContainer.className = "reach-container";
+// creating reach container
+const reachContainer = document.createElement("div");
+reachContainer.className = "reach-container";
 
-// // creating like container
-// const likeContainer = document.createElement("div");
-// likeContainer.className = "like-conatainer";
+// creating like container
+const likeContainer = document.createElement("div");
+likeContainer.className = "like-conatainer";
 
-// // const svgContainer = document.createElement("")
+// const svgContainer = document.createElement("")
 
 
-// // creating views container
-// const viewContainer = document.createElement("div");
-// viewContainer.className = "view-container";
+// creating views container
+const viewContainer = document.createElement("div");
+viewContainer.className = "view-container";
 
 function openNav() {
     document.getElementById("mySidenav").style.height = "100vh";
